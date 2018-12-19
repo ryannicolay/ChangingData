257f3258b84bf79affe97e5fca55d47f3782f11b
Ryan Nicolay <ryannicolay@gmail.com>
2018-12-19 10:27:52 -0500

    Second Commit

---
+++ Vidzy/Classification.cs
@@ -0,0 +1,9 @@
+﻿namespace ChangingData
+{
+    public enum Classification : byte
+    {
+        Silver = 1, 
+        Gold = 2,
+        Platinum = 3
+    }
+}
---
+++ Vidzy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace ChangingData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new VidzyContext();
+
+            var videos = context.Videos.Include(v => v.Genre).ToList();
+
+            foreach (var video in videos)
+            {
+                Console.WriteLine(video.Name);
+                Console.WriteLine("\t" + video.Genre.Name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
